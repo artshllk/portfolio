2fc0d463076385f2d09524d7e5e005c3a4b30c15
ArtSh1 <artshllaku48@gmail.com>
2024-08-27 13:55:29 +0200

    test CI

--- testing/playwright/utils/namespace.js
+++ testing/playwright/utils/namespace.js
@@ -1,5 +1,5 @@
 export function getBaseUrl() {
-  let isProduction = false;
+  let isProduction = process.env.NODE_ENV === "production";
   let productionUrl = "https://artshllaku-portfolio.vercel.app/";
   let defaultLocalUrl = "http://localhost:3000/";
 

9d1fdfca54e3bed9ef3301aaa8896ea7f09f956e
ArtSh1 <artshllaku48@gmail.com>
2024-08-27 14:08:59 +0200

    add webServer in pw configs

--- playwright.config.js
+++ playwright.config.js
@@ -73,9 +73,9 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm dev", // Command to start your local server
+    url: "http://localhost:3000", // URL where your local server is running
+    reuseExistingServer: !process.env.CI,
+  },
 });

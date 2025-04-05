333fc8494807a5fb210cf00c98cd2f4d26d67ab4
artshllk <artshllaku48@gmail.com>
2025-04-05 19:23:53 +0200

    improve about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,14 +2,22 @@ const AboutSection = () => {
   return (
     <>
       <h6>Who I Am</h6>
+
       <p className="text-accent-foreground mt-2">
-        Highly motivated Automation Engineer with a Frontend background and
-        extensive experience working remotely for international companies.
+        I'm a QA Automation Engineer with some Frontend experience (React.js,
+        JavaScript, TypeScript). I work well with remote teams from different
+        countries. I create reliable tests—from full end-to-end tests to small
+        unit tests—and help make sure websites and apps work well, including on
+        mobile using BrowserStack.
       </p>
+
       <p className="text-accent-foreground mt-2">
-        Proven ability to collaborate effectively with cross-functional teams
-        and contribute to innovative solutions. Expertise in testing
-        methodologies and best practices using Playwright, Cypress, and Jest.
+        I enjoy working in Agile teams and use tools like Jira, Trello, Azure
+        DevOps, and ClickUp to stay organized. I work with Playwright, Cypress,
+        Selenium, and Jest to write automated tests, and I also set up GitHub
+        Actions to run tests and deploy code. Whether it’s manual or automated
+        testing, I care about doing things the right way and making sure
+        everything works smoothly.
       </p>
     </>
   );
